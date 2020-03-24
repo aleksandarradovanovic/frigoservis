d89a3dbfb8c0f6dda2e0fdb0bac8957e6d33aa2d
radovanovica <acaca93@gmail.com>
2020-03-24 20:03:21 +0100

    new home component; gallery component; row bug fixed

--- src/app/home/HomeContentContainer.js
+++ src/app/home/HomeContentContainer.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import HomeContainerCarousel from './HomeContainerCarousel';
 import HomeFirstSection from './HomeFirstSection';
 import TextBannerSection from '../components/content/TextBannerSection';
+import HomeSecondSection from './HomeSecondSection';
 
 class HomeContentContainer extends Component {
     render() {
         return (
-            <div className = "homeContentContainer">
+            <div className = "container-fluid homeContentContainer">
+                
                 <div className = "row">
                 <HomeContainerCarousel />
                 </div>
                 <div className = "row">
                     <TextBannerSection />
+                </div>
+         
+                <div className = "row">
+                    <HomeSecondSection />
                 </div>
                 <div className = "row">
                     <HomeFirstSection />
---
+++ src/app/home/HomeSecondSection.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Gallery from 'react-grid-gallery';
+import {img} from '../../../public/image/klimaTest.jpg'
+class HomeSecondSection extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div class="grid">
+          <figure class="effect-layla">
+            <img src={'../../../public/image/klimaTest.jpg'} alt="img06" />
+            <figcaption>
+              <h2>Test <span>Test</span></h2>
+              <p>asgasgasgasgasgas</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+          <figure class="effect-layla">
+            <img src="../../../public/image/test3.jpeg" alt="img03" />
+            <figcaption>
+              <h2>Test <span>Test</span></h2>
+              <p>asgasgasgas</p>
+              <a href="#">View more</a>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeSecondSection;
